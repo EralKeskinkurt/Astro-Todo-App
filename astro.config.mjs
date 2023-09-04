5ab1af568667b919dfe0c93037af5f2c2b2d4c30
Eral <tampermonkey08@gmail.com>
2023-09-04 23:23:22 +0300

    reapired

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // https://astro.build/config
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: 'server',
+  adapter: vercel()
 });
